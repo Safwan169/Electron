e08fd7f82e59201a27ebbebf0d6dd1f07d60125f
safwan169 <safwanhossainmridul@gmail.com>
2025-05-15 12:19:51 +0600

    first commit

---
+++ backend/routes.js
@@ -0,0 +1,9 @@
+const express=require('express')
+const mainRoute=express.Router()
+
+
+
+
+
+
+module.exports = mainRoute
---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl text-center uppercase items-center h-screen justify-center flex'>
+        🚧 UI is now under development
+      </div>
+    </>
+  )
+}
+
+export default App
